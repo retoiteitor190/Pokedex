f4f7962a37511cf7e997646e070a15cb4ce2eca7
retoiteitor190 <63795517+retoiteitor190@users.noreply.github.com>
2022-01-19 16:50:11 -0600

    main

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 
+
 function App() {
   const [pokemon,setPokemon]= useState({});
 
@@ -51,13 +52,26 @@ function App() {
           <img src={pokemon?.sprites?.back_default ?? "https://pngimg.com/uploads/pokeball/pokeball_PNG26.png"} className="poke-image" alt="logo" />
           <img src={pokemon?.sprites?.front_default ?? "https://i.pinimg.com/originals/95/fc/30/95fc304b40461a9922bd1d3aff885496.png"} className="poke-image" alt="logo" />
         </div>
-        <h3>Nombre del pokemon</h3>
+        <h3>pokemon names</h3>
         <p> {pokemon.name ?? "No pokemon Selected"}</p>
+        <h3>Id Pokemon</h3>
         <p>{pokemon.id}</p>
+        <div>
+          <ul className='text'>
+          {
+            pokemon?.abilities?.map((ability) => (
+              <li key={ability.ability.id}> {ability.ability.name}  </li>
+            ))
+          }
+          </ul>
+          </div>
         <div className='flex-container'>
           <button className='button' onClick={()=>fetchPokemon(back())}>Back</button>
           <button className='button' onClick={()=>fetchPokemon(getRandomInt())}>Random</button>
           <button className='button' onClick={()=>fetchPokemon(next())}>Next</button>
+        </div>
+        <div>
+          
         </div>
       </header>
     </div>
