c4c90bb4c7a09757275b94a1882f5bacbdbd2298
retoiteitor190 <63795517+retoiteitor190@users.noreply.github.com>
2022-01-18 22:24:08 -0600

    main

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [pokemon,setPokemon]= useState({});
+
+  const fetchPokemon=(id)=>{
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then((response)=>response.json())
+    .then((data)=>setPokemon(data));
+  };
+
+  const getRandomInt=(min= 1,max=500)=>{
+    return Math.floor(Math.random()* (max-min)+min);
+  }
+
+
+  useEffect(()=>{
+    console.log({pokemon});
+  },[pokemon]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='flex-container'>
+          <img src={logo} className="poke-image" alt="logo" />
+          <img src={logo} className="poke-image" alt="logo" />
+        </div>
+        <p> Name</p>
+        <div className='flex-container'>
+          <button className='button'>Back</button>
+          <button className='button' onClick={()=>fetchPokemon(getRandomInt())}>Random</button>
+          <button className='button'>Next</button>
+        </div>
       </header>
     </div>
   );
