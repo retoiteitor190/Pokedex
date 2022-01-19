ae0dd51634fcc6b63730f9eac0fa13afd9820f27
retoiteitor190 <63795517+retoiteitor190@users.noreply.github.com>
2022-01-18 23:28:42 -0600

    main

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import { useEffect, useState } from 'react';
 function App() {
   const [pokemon,setPokemon]= useState({});
 
+  const[actual,setActual]= useState(false);
+
+  const toggle=()=>{
+    setActual(!actual)
+  }
   const fetchPokemon=(id)=>{
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
     .then((response)=>response.json())
@@ -15,6 +20,12 @@ function App() {
     return Math.floor(Math.random()* (max-min)+min);
   }
 
+  const next=()=>{
+    return pokemon.id +1;
+  }
+  const back=()=>{
+    return pokemon.id - 1;
+  }
 
   useEffect(()=>{
     console.log({pokemon});
@@ -23,14 +34,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className='flex-container'>
-          <img src={logo} className="poke-image" alt="logo" />
-          <img src={logo} className="poke-image" alt="logo" />
+          <img src={pokemon?.sprites?.back_default ?? "https://pngimg.com/uploads/pokeball/pokeball_PNG26.png"} className="poke-image" alt="logo" />
+          <img src={pokemon?.sprites?.front_default ?? "https://i.pinimg.com/originals/95/fc/30/95fc304b40461a9922bd1d3aff885496.png"} className="poke-image" alt="logo" />
         </div>
-        <p> Name</p>
+        <a href='https://github.com/retoiteitor190/Pokedex.git'>Git</a>
+        <p> {pokemon.name ?? "No pokemon Selected"}</p>
+        <p>{pokemon.id}</p>
         <div className='flex-container'>
-          <button className='button'>Back</button>
+          <button className='button' onClick={()=>fetchPokemon(back())}>Back</button>
           <button className='button' onClick={()=>fetchPokemon(getRandomInt())}>Random</button>
-          <button className='button'>Next</button>
+          <button className='button' onClick={()=>fetchPokemon(next())}>Next</button>
         </div>
       </header>
     </div>
